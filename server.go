2f518816674d354126e266fac7b2829e20777b7b
Andrew Allen <andrewallen2012@gmail.com>
2014-04-27 15:39:18 -0500

    initial commit, following jakecoffman tutorial

---
+++ server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "github.com/gorilla/websocket"
+  "log"
+  "net/http"
+)
+
+var connections map[*websocket.Conn]bool
+
+func sendAll(msg []byte) {
+  for conn := range connections {
+    if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+      delete(connections, conn)
+      conn.Close()
+    }
+  }
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+  // Taken from gorilla's website
+  conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+  if _, ok := err.(websocket.HandshakeError); ok {
+    http.Error(w, "Not a websocket handshake", 400)
+    return
+  } else if err != nil {
+    log.Println(err)
+    return
+  }
+  log.Println("Succesfully upgraded connection")
+  connections[conn] = true
+
+  for {
+    // Blocks until a message is read
+    _, msg, err := conn.ReadMessage()
+    if err != nil {
+      delete(connections, conn)
+      conn.Close()
+      return
+    }
+    log.Println(string(msg))
+    sendAll(msg)
+  }
+}
+
+func main() {
+  port := flag.Int("port", 80, "port to serve on")
+  dir := flag.String("directory", "www/", "directory of web files")
+  flag.Parse()
+
+  connections = make(map[*websocket.Conn]bool)
+
+  // handle all requests by serving a file of the same name
+  fs := http.Dir(*dir)
+  fileHandler := http.FileServer(fs)
+  http.Handle("/", fileHandler)
+  http.HandleFunc("/ws", wsHandler)
+
+  log.Printf("Running on port     %d\n", *port)
+
+  addr := fmt.Sprintf("127.0.0.1:%d", *port)
+  // this call blocks -- the progam runs here forever
+  err := http.ListenAndServe(addr, nil)
+  fmt.Println(err.Error())
+}
